08dcfab324d3751b9d90d19dc4b45f4c6121e6ff
eytan-ohana <eytan.ohana@traxretail.com>
2022-06-08 02:38:56 +0300

    rename vars

--- app/utils/clt.py
+++ app/utils/clt.py
@@ -16,13 +16,12 @@ def update_distribution(i):
     st.session_state[CltConsts.PROB] = probs
 
 
-def generate_sample_means(sample_size, sample_means):
+def generate_sample_means(sample_size, n_samples):
     if len(st.session_state[CltConsts.DIST_VALS]) == 0:
         st.error('')
         return
     with st.spinner('Generating sample means'):
         sample_means = np.array([np.random.choice(st.session_state[CltConsts.DIST_VALS],
-                                                  p=st.session_state[CltConsts.PROB],
-                                                  size=sample_size).mean()
-                                 for _ in range(sample_means)])
-    return sample_means
+                                               p=st.session_state[CltConsts.PROB],
+                                               size=sample_size).mean()
+                              for _ in range(n_samples)])
