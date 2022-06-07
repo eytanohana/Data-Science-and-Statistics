2978a153c5a98319fccae4848739ec01c55a342c
eytan-ohana <eytan.ohana@traxretail.com>
2022-06-08 02:39:58 +0300

    error msg

--- app/utils/clt.py
+++ app/utils/clt.py
@@ -19,7 +19,7 @@ def update_distribution(i):
 
 def generate_sample_means(sample_size, n_samples):
     if len(st.session_state[CltConsts.DIST_VALS]) == 0:
-        st.error('')
+        st.error('You must create a distribution before generating sample means.')
         return
     with st.spinner('Generating sample means'):
         sample_means = np.array([np.random.choice(st.session_state[CltConsts.DIST_VALS],
