5ff8c9719cfd2a76c10af0f58369141b2293e290
eytan-ohana <eytan.ohana@traxretail.com>
2022-06-08 02:02:29 +0300

    calc mean and std

--- app/pages/Central_Limit_Theorem.py
+++ app/pages/Central_Limit_Theorem.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from app.utils.clt import update_distribution, CltConsts
 
 import streamlit as st
@@ -15,18 +17,24 @@ st.markdown('''The central theorem limit states that for *almost* any distributi
 st.subheader('Create a random distribution')
 if CltConsts.DIST not in st.session_state:
     st.session_state[CltConsts.VALUES] = []
-    st.session_state[CltConsts.DIST] = {CltConsts.VALUES: [0], CltConsts.PROB: [0]}
+    st.session_state[CltConsts.DIST] = {CltConsts.VALUES: np.array([0]), CltConsts.PROB: np.array([0])}
 
 n_dist_values = 20
 dist_value_cols = st.columns(n_dist_values)
 for i, dist_col in enumerate(dist_value_cols, 1):
     dist_col.button(f'{i}', on_click=update_distribution, args=(i,))
 
+mean = np.sum(st.session_state[CltConsts.DIST][CltConsts.VALUES]
+              * st.session_state[CltConsts.DIST][CltConsts.PROB])
+std = np.sqrt(np.sum(st.session_state[CltConsts.DIST][CltConsts.VALUES]**2 *
+                     st.session_state[CltConsts.DIST][CltConsts.PROB]) - mean**2)
+
 st.plotly_chart(
     px.bar(
         x=st.session_state[CltConsts.DIST][CltConsts.VALUES],
         y=st.session_state[CltConsts.DIST][CltConsts.PROB],
         range_x=(0, n_dist_values + 1),
-        labels={'x': 'k', 'y': 'P(x=k)'}
+        labels={'x': 'k', 'y': 'P(x=k)'},
+        title=f'Distribution with mean {mean:.3f} and standard deviation {std:.3f}'
     )
 )
