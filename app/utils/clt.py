9049a6e969cf1a88e8b3ebd2124e75292dfb2a4e
eytanohana <eohana98@gmail.com>
2022-06-10 00:44:21 +0300

    rename var

--- app/utils/clt.py
+++ app/utils/clt.py
@@ -5,7 +5,7 @@ import streamlit as st
 class CltConsts:
     VALUES = 'values'
     DIST_VALS = 'dist_vals'
-    PROB = 'prob'
+    PROBS = 'probs'
     SAMPLE_MEANS = 'sample_means'
 
 
@@ -14,7 +14,7 @@ def update_distribution(i):
     values, counts = np.unique(st.session_state[CltConsts.VALUES], return_counts=True)
     probs = counts / counts.sum()
     st.session_state[CltConsts.DIST_VALS] = values
-    st.session_state[CltConsts.PROB] = probs
+    st.session_state[CltConsts.PROBS] = counts / counts.sum()
 
 
 def generate_sample_means(sample_size, n_samples):
@@ -23,7 +23,7 @@ def generate_sample_means(sample_size, n_samples):
         return
     with st.spinner('Generating sample means'):
         sample_means = np.array([np.random.choice(st.session_state[CltConsts.DIST_VALS],
-                                               p=st.session_state[CltConsts.PROB],
-                                               size=sample_size).mean()
-                              for _ in range(n_samples)])
+                                                  p=st.session_state[CltConsts.PROBS],
+                                                  size=sample_size).mean()
+                                 for _ in range(n_samples)])
     st.session_state[CltConsts.SAMPLE_MEANS] = sample_means
