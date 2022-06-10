e6668d4e414b81459f6f99c504d45d795da3a29c
eytanohana <eohana98@gmail.com>
2022-06-10 15:29:54 +0300

    replace clt objects

--- app/pages/Central_Limit_Theorem.py
+++ app/pages/Central_Limit_Theorem.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from app.utils.clt import CltConsts, update_distribution, generate_sample_means
+from app.utils import clt
 
 import streamlit as st
 import plotly.express as px
@@ -14,26 +14,24 @@ st.markdown('''The central theorem limit states that for *almost* any distributi
                of size n that as we increase the sample size n, the distribution of the sample 
                means will look more and more normal.''')
 
-st.subheader('Create a random distribution')
-if CltConsts.DIST_VALS not in st.session_state:
-    st.session_state[CltConsts.VALUES] = []
-    st.session_state[CltConsts.DIST_VALS] = np.array([])
-    st.session_state[CltConsts.PROB] = np.array([])
-
 n_dist_values = 20
+st.subheader(f'Create a random distribution with {n_dist_values} distinct possible values')
+if clt.DIST_VALS not in st.session_state:
+    st.session_state[clt.VALUES] = []
+    st.session_state[clt.DIST_VALS] = np.array([])
+    st.session_state[clt.PROBS] = np.array([])
+
 dist_value_cols = st.columns(n_dist_values)
 for i, dist_col in enumerate(dist_value_cols, 1):
-    dist_col.button(f'{i}', on_click=update_distribution, args=(i,))
+    dist_col.button(f'{i}', on_click=clt.update_distribution, args=(i,))
 
-mean = np.sum(st.session_state[CltConsts.DIST_VALS]
-              * st.session_state[CltConsts.PROB])
-std = np.sqrt(np.sum(st.session_state[CltConsts.DIST_VALS] ** 2 *
-                     st.session_state[CltConsts.PROB]) - mean**2)
+mean = clt.get_distribution_mean()
+std = clt.get_distribution_std()
 
 st.plotly_chart(
     px.bar(
-        x=st.session_state[CltConsts.DIST_VALS],
-        y=st.session_state[CltConsts.PROB],
+        x=st.session_state[clt.DIST_VALS],
+        y=st.session_state[clt.PROBS],
         range_x=(0, n_dist_values + 1),
         labels={'x': 'k', 'y': 'P(x=k)'},
         title=f'μ = {mean:.3f} σ = {std:.3f}'
@@ -45,4 +43,4 @@ st.markdown('---')
 sample_size = st.number_input('Sample size', 1, 100, value=5)
 sample_means = st.number_input('Number of samples', 100, 10_000, value=500)
 
-st.button('Generate sample means', on_click=generate_sample_means, args=(sample_size, sample_means))
+st.button('Generate sample means', on_click=clt.generate_sample_means, args=(sample_size, sample_means))
