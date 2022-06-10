646710fe4009c4328c4c3d93ec1ca39c3beb75d8
eytanohana <eohana98@gmail.com>
2022-06-10 16:08:17 +0300

    rm spinner from clt backend

--- app/backend/clt.py
+++ app/backend/clt.py
@@ -30,11 +30,10 @@ def generate_sample_means(sample_size, n_samples):
     if len(st.session_state[VALUES]) == 0:
         st.error('You must create a distribution before generating sample means.')
         return
-    with st.spinner(f'Generating {n_samples} sample means'):
-        sample_means = np.array([np.random.choice(st.session_state[DIST_VALS],
-                                                  p=st.session_state[PROBS],
-                                                  size=sample_size).mean()
-                                 for _ in range(n_samples)])
+    sample_means = np.array([np.random.choice(st.session_state[DIST_VALS],
+                                              p=st.session_state[PROBS],
+                                              size=sample_size).mean()
+                             for _ in range(n_samples)])
     st.session_state[SAMPLE_MEANS] = sample_means
 
 
