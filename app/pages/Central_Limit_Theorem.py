985e3666b36249eb305615a6988b3e9ddde2d087
eytanohana <eohana98@gmail.com>
2022-06-10 17:25:40 +0300

    quote paragaph

--- app/pages/Central_Limit_Theorem.py
+++ app/pages/Central_Limit_Theorem.py
@@ -10,10 +10,10 @@ st.set_page_config(page_title='CLT', page_icon='📉', layout='wide')
 
 st.header('The Central Limit Theorem')
 
-st.markdown('''The central theorem limit states that for *almost* any distribution with mean,
-               $\mu$, and standard deviation, $\sigma$, if we take the means sufficiently many samples 
-               of size n that as we increase the sample size n, the distribution of the sample 
-               means will look more and more normal.''')
+st.markdown('>**The central theorem limit states that for *almost* any distribution with mean, '
+            '$\mu$, and standard deviation, $\sigma$, if we take the means sufficiently many samples '
+            'of size n that as we increase the sample size n, the distribution of the sample '
+            'means will look more and more normal.**')
 
 n_dist_values = 20
 st.subheader(f'Create a random distribution from {n_dist_values} distinct possible values')
