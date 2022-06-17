371af04c839af377f0b01cdfabe99735b03bb04b
eytanohana <eohana98@gmail.com>
2022-06-17 12:57:36 +0300

    add explanation

--- app/pages/Covariance_and_Correlation.py
+++ app/pages/Covariance_and_Correlation.py
@@ -3,3 +3,16 @@ import streamlit as st
 st.set_page_config(page_title='Data Science & Stats', page_icon='📉', layout='wide')
 
 st.header('Covariance and Correlation')
+
+
+st.markdown('''Covariance and Correlation describe the relationship between two variables.
+
+>If two variables move in the same direction, we say they have a positive relationship.
+>ie when one variable increases and the other does too, vice versa.
+>
+>If they move in opposite directions, then we say they have a negative relationship.
+>ie when one variable increases and the other decreases, vice versa.
+>
+>And if they dont move together at all then we say they have no relationship.
+>ie when one variable increases and the other one either increases or decreases.
+''')
