cc1b26399df2594fb91355da08315661013400b1
eytanohana <eohana98@gmail.com>
2022-06-17 20:10:50 +0300

    test

--- streamlit_app/__init__.py
+++ streamlit_app/__init__.py
@@ -4,4 +4,4 @@ import os
 APP_DIR = os.path.dirname(__file__)
 APP_BACKEND = os.path.join(APP_DIR, 'backend')
 BASE = os.path.dirname(APP_DIR)
-sys.path.append(BASE)
+sys.path.extend([BASE, APP_DIR, APP_BACKEND])
