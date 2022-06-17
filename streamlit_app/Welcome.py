fe13163ad03b043e7d371a8417085bfe4522711c
eytanohana <eohana98@gmail.com>
2022-06-17 19:33:28 +0300

    test

--- streamlit_app/Welcome.py
+++ streamlit_app/Welcome.py
@@ -1,11 +1,4 @@
 import streamlit as st
-import sys
-import os
-
 
 st.set_page_config(page_title='Data Science & Statistics', page_icon='📉', layout='centered')
 st.header('Welcome!')
-
-sys.path.extend([os.path.dirname(__file__), f'{os.path.dirname(__file__)}/backend'])
-
-st.write(sys.path)
