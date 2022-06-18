7f9454f8021d5e218f33b1b29528b172abfdaa7b
eytanohana <eohana98@gmail.com>
2022-06-18 20:27:30 +0300

    fix welcome

--- streamlit_app/Welcome.py
+++ streamlit_app/Welcome.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.set_page_config(page_title='Data Science & Statistics', page_icon='📉', layout='centered')
+st.set_page_config(page_title='Data Science & Stats', page_icon='📉', layout='centered')
 st.header('Welcome!')
-
-import seaborn as sns
