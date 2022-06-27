2920768cd0ab1e8a55c97311832c932ae9a33a02
eytanohana <eohana98@gmail.com>
2022-06-27 18:45:57 +0300

    add markdown

---
+++ app/pages/Common_Distributions.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(page_title='Data Science & Stats', page_icon='📉', layout='wide')
+
+
+st.markdown('''
+# Distributions
+A **Distribution** is just a set of values and the probabilities of getting those values.
+From that we know that all the probabilities in the distribution should sum up to 100%.
+There are two types of distributions.
+
+**Discrete Distributions** are distributions that contain only discrete values like 1, 2, 3, ...
+
+**Continuous Distributions** are distributions that can contain any value like 1.3 or 13.059
+
+## Common Discrete Distributions
+''')
+
