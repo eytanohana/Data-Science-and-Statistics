4f37aaeaefa3f0c3ab717aa6e6b3f3a46eec5100
eytanohana <eohana98@gmail.com>
2022-06-28 02:01:53 +0300

    add binom graph

--- app/backend/dist.py
+++ app/backend/dist.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.stats import binom
 
 PROPS = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 
@@ -12,7 +13,8 @@ class Uniform:
         k = np.arange(a, b+1)
         pmf = np.ones_like(k) / (b - a + 1)
         fig = plt.figure(figsize=(5, 2))
-        plt.xticks(np.arange(a, b+1))
+        plt.xticks(np.arange(a, b+1), fontsize=5)
+        plt.yticks(fontsize=5)
         sns.barplot(k, pmf, color='cadetblue')
         text = f'E(X) = {cls.expectation(a, b):.2f}\nV(X) = {((b-a+1)**2-1)/12:.2f}'
         plt.text(0.01, 0.95, text, transform=plt.gca().transAxes, fontsize=10,
@@ -29,3 +31,31 @@ class Uniform:
     @staticmethod
     def variance(a, b):
         return ((b - a + 1) ** 2 - 1) / 12
+
+
+class Binomial:
+
+    @classmethod
+    def plot_dist(cls, n, p):
+        dist = binom(n, p)
+        k = np.arange(0, n+1)
+        pmf = dist.pmf(k)
+        fig = plt.figure(figsize=(5, 2))
+        sns.barplot(k, pmf, color='cadetblue')
+        text = f'E(X) = {cls.expectation(n, p):.2f}\nV(X) = {cls.variance(n, p)}'
+        plt.xticks(fontsize=5)
+        plt.yticks(fontsize=5)
+        plt.text(0.01, 0.95, text, transform=plt.gca().transAxes, fontsize=10,
+                 verticalalignment='top', bbox=PROPS)
+        plt.grid(ls='dashed')
+        plt.title(f'Binomial distribution with {n=} and {p=}', fontsize=15)
+        plt.ylim(0, 1)
+        return fig
+
+    @staticmethod
+    def expectation(n, p):
+        return n * p
+
+    @staticmethod
+    def variance(n, p):
+        return n * p * (1 - p)
