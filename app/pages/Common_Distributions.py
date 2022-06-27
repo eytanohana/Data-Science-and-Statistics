0f6c84086bb19dc8c1f683be3cf0a2ea29e12a70
eytanohana <eohana98@gmail.com>
2022-06-28 02:08:56 +0300

    add hypergeometric header

--- app/pages/Common_Distributions.py
+++ app/pages/Common_Distributions.py
@@ -26,7 +26,6 @@ a, b, c = st.columns(3)
 a.markdown('$P(X=k)=\\frac{1}{b-a+1}\;\\textrm{, for } k = a, ..., b$')
 b.markdown('$E(X) = \\frac{a+b}{2}$')
 c.markdown('$V(X) = \\frac{(b - a + 1)^2 - 1}{12}$')
-
 a, b = st.columns(2)
 start = a.slider('a', min_value=1, max_value=5, value=1)
 end = b.slider('b', min_value=6, max_value=10, value=6)
@@ -46,3 +45,18 @@ a, b = st.columns(2)
 n = a.slider('n', min_value=0, max_value=30, value=15)
 p = b.slider('p', min_value=0.0, max_value=1.0, value=0.5)
 st.pyplot(dist.Binomial.plot_dist(n, p))
+
+st.markdown('''
+---
+###  The Hypergeometric Distribution $X \sim HG(M, n, N)$
+> X is a random variable that counts the number of "special" items taken from
+>choosing $N$ times out of a total population size of $M$ without replacement.
+''')
+# a, b, c = st.columns(3)
+# a.markdown('$P(X=k)={n \choose k} p^k (1-p)^{n-k}\;\\textrm{, for } k = 0, 1,  ..., n$')
+# b.markdown('$E(X) = np$')
+# c.markdown('$V(X) = np(1-p)$')
+# a, b = st.columns(2)
+# n = a.slider('n', min_value=0, max_value=30, value=15)
+# p = b.slider('p', min_value=0.0, max_value=1.0, value=0.5)
+# st.pyplot(dist.Binomial.plot_dist(n, p))
