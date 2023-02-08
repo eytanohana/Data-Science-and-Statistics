5cedb8d0f3df110388f0be5a1dd82c434a866fc2
eytanohana <eohana98@gmail.com>
2023-02-09 00:37:44 +0200

    remove warnings

--- app/Welcome.py
+++ app/Welcome.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from warnings import filterwarnings
 
+filterwarnings(action='ignore')
 st.set_page_config(page_title='Data Science & Stats', page_icon='📉', layout='centered')
 st.header('Welcome!')
