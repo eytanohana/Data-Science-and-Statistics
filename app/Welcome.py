a2a3ed2af50b03963ecc9d1076840d2a12a905c7
eytanohana <eohana98@gmail.com>
2023-09-02 18:48:06 +0300

    logger

--- app/Welcome.py
+++ app/Welcome.py
@@ -6,6 +6,8 @@ import os
 
 filterwarnings(action='ignore')
 st.set_page_config(page_title='Data Science & Stats', page_icon='📉', layout='centered')
+logger = st.logger.get_logger(__name__)
+
 st.header('Welcome!')
 
-print(f"Current Version: {os.environ.get('DOCKER_TAG')}")
+logger.info(f"Current Version: {os.environ.get('DOCKER_TAG')}")
